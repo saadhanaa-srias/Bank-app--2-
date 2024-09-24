bd8e4f17abd8bdcba6821b506281855dd145350c
saadhanaa-srias <saadhanaasrias@gmail.com>
2024-09-24 18:56:03 +0530

    L1

---
+++ script.js
@@ -0,0 +1,5 @@
+var credit_balance=parseInt(current_balance_el.innerText) + parseInt(entered_amount_el)
+    document.getElementById("credit").innerHTML;
+  
+  var debit_balance=parseInt(current_balance_el.innerText) - parseInt(entered_amount_el)
+    document.getElementById("debit").innerHTML;
